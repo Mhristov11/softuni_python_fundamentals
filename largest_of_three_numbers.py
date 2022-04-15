b5cf488fb0e9c40245323e66b7c13f9f268ee972
Martin Hristov <mhristovmail@gmail.com>
2022-04-15 12:37:51 +0300

    code update

--- largest_of_three_numbers.py
+++ largest_of_three_numbers.py
@@ -1,23 +1,10 @@
-# import sys
-#
-# max_number=-sys.maxsize
-#
-# for x in range (3):
-#     number=int(input())
-#     if number > max_number:
-#         max_number=number
-#
-# print(max_number)
+import sys
 
+max_number=-sys.maxsize
 
+for x in range (3):
+    number=int(input())
+    if number > max_number:
+        max_number=number
 
-n_one=int(input())
-n_two=int(input())
-n_three=int(input())
-
-if n_one > n_two and n_one > n_three:
-    print(n_one)
-elif n_two > n_three and n_two > n_one:
-    print(n_two)
-else:
-    print(n_three)
+print(max_number)
