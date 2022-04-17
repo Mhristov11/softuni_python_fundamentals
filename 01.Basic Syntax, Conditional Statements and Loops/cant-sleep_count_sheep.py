64e9313d024d96a886dfb4bd682ed166239e986e
Martin Hristov <mhristovmail@gmail.com>
2022-04-17 22:38:24 +0300

    New file

---
+++ 01.Basic Syntax, Conditional Statements and Loops/cant-sleep_count_sheep.py
@@ -0,0 +1,8 @@
+#If you can't fall asleep, try counting sheep! You will be given a positive integer, 3, for example.
+# You should return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid,
+# i.e., integers greater than 0.
+
+n = int(input())
+
+for x in range (1,n+1):
+    print(f"{x} sheep...", end="")
