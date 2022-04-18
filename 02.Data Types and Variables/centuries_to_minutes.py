c0a61eb8358e6cb92ad622cf7dc6845797dc6d51
Martin Hristov <mhristovmail@gmail.com>
2022-04-18 21:42:53 +0300

    New file

---
+++ 02.Data Types and Variables/centuries_to_minutes.py
@@ -0,0 +1,12 @@
+# Write a program that reads an integer number of centuries and converts it to years, days, hours, and minutes
+# Assume that one year has 365.2422 days on average
+
+from math import floor
+
+centuries = int(input())
+years = centuries * 100
+days = floor(years * 365.2422)
+hours = days * 24
+minutes = hours * 60
+
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
