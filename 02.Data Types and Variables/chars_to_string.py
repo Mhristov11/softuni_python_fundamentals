93508c5cc9e6ed99e6f28aa13151188d5c2a1ae1
Martin Hristov <mhristovmail@gmail.com>
2022-04-27 12:39:07 +0300

    New file

---
+++ 02.Data Types and Variables/chars_to_string.py
@@ -0,0 +1,4 @@
+# Write a function that receives 3 characters.
+# Concatenate all the characters into one string and print it on the console.
+a, b, c, = input(), input(), input()
+print(f"{a}{b}{c}")
