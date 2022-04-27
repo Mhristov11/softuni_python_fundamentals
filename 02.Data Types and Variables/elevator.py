016b6ab1a903e54e87f24fecafa7f238073fec84
Martin Hristov <mhristovmail@gmail.com>
2022-04-27 12:39:43 +0300

    New file

---
+++ 02.Data Types and Variables/elevator.py
@@ -0,0 +1,8 @@
+# Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons.
+# The input holds two lines: the number of people N and the capacity P of the elevator.
+import math
+number_of_people = int(input())
+capacity = int(input())
+
+courses = math.ceil(number_of_people / capacity)
+print(courses)
