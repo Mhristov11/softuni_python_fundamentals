1f943a948a6e599980c1a99de0e9f7855d888fa0
Martin Hristov <mhristovmail@gmail.com>
2022-04-27 12:41:09 +0300

    New file

---
+++ 02.Data Types and Variables/integer_operations.py
@@ -0,0 +1,10 @@
+# Write a program that reads four integer numbers. It should add the first to the second number,
+# integer divide the sum# by the third number, and multiply the result by the fourth number.
+# Print the final result.
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+
+total = int((n1 + n2) / n3) * n4
+print(total)
