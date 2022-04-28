9199b3bfad07c092a0f5e5736770f4098f4bc163
Martin Hristov <mhristovmail@gmail.com>
2022-04-28 12:34:09 +0300

    New file

---
+++ 03.Lists Basics/courses.py
@@ -0,0 +1,9 @@
+# On the first line, you will receive a single number n. On the following n lines, you will receive names of courses.
+# You should create a list of courses and print it.
+
+n = int(input())
+list = []
+for _ in range(n):
+    list.append(input())
+
+print(list)
