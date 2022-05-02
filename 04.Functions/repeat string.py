843794ff4652b27e76c34fe46e799edfe9ec8e22
Martin Hristov <mhristovmail@gmail.com>
2022-05-02 19:29:52 +0300

    New file

---
+++ 04.Functions/repeat string.py
@@ -0,0 +1,9 @@
+# Write a function that receives a string and a counter n.
+# The function should return a new string – the result of repeating the old string n times.
+# Print the result of the function. Try using lambda.
+
+new_string = lambda string, a: print(string * n)
+
+str = input()
+n = int(input())
+new_string(str, n)
