129b051ed73f3f234f1f753cf5a5c982f66c1770
Martin Hristov <mhristovmail@gmail.com>
2022-05-04 22:49:29 +0300

    New file

---
+++ 04.Functions/even_numbers.py
@@ -0,0 +1,6 @@
+# Write a program that receives a sequence of numbers (integers) separated by a single space.
+# It should print a list of only the even numbers.
+
+result = list(filter(lambda x: (x % 2 == 0), list(map(int, input().split(" ")))))
+
+print(result)
